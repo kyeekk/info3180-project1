8ae3e4fc0fe6c21c192fa87011b6f1748e226a87
Kayla Effs <kaylaeffs@gmail.com>
2019-03-19 03:44:04 +0000

    updated to work with heroku

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "65382K6280Y2790E"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://zbdkquvmxzckhq:6e785fa03d668d114af64f9bd8c439aaf15546c88cbb3b6cc0ca0785561be927@ec2-54-221-243-211.compute-1.amazonaws.com:5432/d5n09via8sdn3c"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 
